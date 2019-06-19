833a276f085adbd7f5760522868dad25d818526a
TIMUR <1tkurbanov@gmail.com>
2019-06-19 15:01:38 -0400

    done

--- first_ajax/static/js/main.js
+++ first_ajax/static/js/main.js
@@ -1,3 +1,56 @@
 document.addEventListener("DOMContentLoaded", function() {
-  
+    
+    const rootButton = document.getElementById('main');
+    rootButton.addEventListener('click', function() {
+        request = axios.get('http://intro-ajax-api.herokuapp.com');
+    });
+    const section3456 = document.getElementById('steps3456');
+    const section7 = document.getElementById('step7');
+    
+    
+    const pingPongButton = document.getElementById('ping_pong');
+    pingPongButton.addEventListener('click', function() {
+        request = axios.get('http://intro-ajax-api.herokuapp.com/pong')
+        .then(function(resp) {
+            console.log(resp.data);
+            section3456.append(resp.data);
+        })
+        .catch(function(error) {
+            console.log('Sorry for the trouble', error)
+            section3456.append('I will try harder next time.')
+        })
+        .then(function(){
+            console.log('Hey the request finished!')
+        });
+    });
+    const sharedButton = document.getElementById('shared');
+    sharedButton.addEventListener('click', function() {
+        request = axios.get('http://intro-ajax-api.herokuapp.com/count', {
+        params: {
+            amount: 3
+        }
+    })
+    .then(function(resp) {
+        console.log("Current count is " + resp.data);
+        section7.append(resp.data);
+    })
+})
+    const requestTimeButton = document.getElementById('request_time');
+    requestTimeButton.addEventListener('click', function() {
+    request = axios.get('http://intro-ajax-api.herokuapp.com/time', {params: {timezone: 'America/Mexico_City'}})
+    .then(function(resp) {
+        console.log(resp.data)
+        section8.append(resp.data);
+    })
+});
+const carButton = document.getElementById('car');
+const carList = document.getElementById('list');
+carButton.addEventListener('click', function(){
+    request = axios.get('http://intro-ajax-api.herokuapp.com/a_car')
+    .then(function(resp){
+        const newCar = document.createElement('li');
+        newCar.innerHTML = resp.data;
+        carList.append(newCar);
+    })
+})
 });
